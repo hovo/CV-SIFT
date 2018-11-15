d49c4547cb1ada428f6be03a1a4309fd8ec7c440
Hovo <hovo.khc@gmail.com>
2018-11-14 20:17:21 -0500

    find min/max pixels from the kernel

--- SSExtrema.m
+++ SSExtrema.m
@@ -3,14 +3,37 @@ function kp = SSExtrema(DoGPyr)
 noctaves = length(DoGPyr);
 kp = cell(1, noctaves);
 
-% find number of DoG at each octave
-[~,~,ns] = size(DoGPyr{1}); 
-
 for octaveIdx=1:noctaves
+    % Get current octave
     octave = DoGPyr{octaveIdx};
+    % Find size of the octave
+    [row,col,ns] = size(octave); 
     for dogIdx = 2:(ns-1)
+        % Get the 3 DoGs
         currentDog = octave(:,:,dogIdx);
         topDog = octave(:,:,dogIdx+1);
-        bottonDog = octave(:,:,dogIdx-1);
+        bottomDog = octave(:,:,dogIdx-1);
+        
+        % Iterate over the current DoG
+        for rowIdx=2:row-1
+            for colIdx=2:col-1
+                pixel = currentDog(rowIdx, colIdx);
+                kernel(1:3,1:3,1) = topDog(rowIdx-1:rowIdx+1, colIdx-1:colIdx+1);
+                kernel(1:3,1:3,2) = currentDog(rowIdx-1:rowIdx+1, colIdx-1:colIdx+1);
+                kernel(1:3,1:3,3) = bottomDog(rowIdx-1:rowIdx+1, colIdx-1:colIdx+1);
+                
+                % Find the min and max pixels from the kernel
+                minNeighbour = min(min(kernel));
+                maxNeighbour = max(max(kernel));
+                
+                if(pixel == minNeighbour)
+                    % TODO: Save
+                end
+                
+                if(pixel == maxNeighbour)
+                    % TODO: Save
+                end
+            end
+        end
     end
 end
